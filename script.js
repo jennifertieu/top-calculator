586ff9a2536c82bcc4678ebc611fbad591f758f2
Jennifer Tieu <jennifer.tieuu@gmail.com>
2022-03-21 20:36:14 -0500

    create calculate function, capture button values

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-"use strict"
+(function(window, document, undefined){
+    "use strict";
+    let values = [];
+    const display = document.querySelector(".display");        
+    const buttons = document.querySelectorAll(".button");
+    buttons.forEach((button) => button.addEventListener("click", calculate));
+
+    function calculate(event){
+        let buttonValue = event.target.value;
+        // save button values until equal operator is used
+        console.log(buttonValue);
+        if (event.target.value === "="){
+            // calculate values 
+            console.log(values);
+            // return value 
+            return;
+        } else if (buttonValue == "C"){
+            // clear values 
+            values = [];
+            return;
+        }
+        // when equal button is used, calculate values
+        values.push(buttonValue);
+        display.textContent = buttonValue;
+    }
+
+})(window, document);
