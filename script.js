ca0be2f484fd24df2ed610afaefc17dfad9247e4
Jennifer Tieu <jennifer.tieuu@gmail.com>
2022-03-21 23:05:24 -0500

    add functions for math operations, operate, operator, and operands

--- script.js
+++ script.js
@@ -1,27 +1,84 @@
 (function(window, document, undefined){
     "use strict";
-    let values = [];
-    const display = document.querySelector(".display");        
-    const buttons = document.querySelectorAll(".button");
-    buttons.forEach((button) => button.addEventListener("click", calculate));
+    let result = 0;
+    let inputs = [];
+    const display = document.querySelector(".display");   
+    display.textContent = +result;
 
-    function calculate(event){
+    const buttonOperand = document.querySelectorAll(".button-operand");
+    buttonOperand.forEach((button) => button.addEventListener("click", displayNumbers));
+
+    const buttonOperator = document.querySelectorAll(".button-operator");
+    buttonOperator.forEach((button) => button.addEventListener("click", operator));
+    
+    function displayNumbers(event){
         let buttonValue = event.target.value;
-        // save button values until equal operator is used
         console.log(buttonValue);
-        if (event.target.value === "="){
-            // calculate values 
-            console.log(values);
-            // return value 
-            return;
-        } else if (buttonValue == "C"){
-            // clear values 
-            values = [];
-            return;
-        }
-        // when equal button is used, calculate values
-        values.push(buttonValue);
-        display.textContent = buttonValue;
+        result += buttonValue;
+        display.textContent = +result;
     }
 
+    function operator(event){
+        let buttonValue = event.target.value;
+        inputs.push(+result);
+        console.log(inputs);
+        if (buttonValue === "="){
+            // calculate values 
+            console.log(inputs);
+            display.textContent = operate(inputs);
+            return;
+        } else if (buttonValue === "C"){
+            // clear inputs 
+            inputs = [];
+            result = 0;
+            display.textContent = result;
+            return;
+        } else if (inputs[2]){
+            display.textContent = operate(inputs); 
+        } else if (!inputs[1]){
+            // add operator
+            inputs.push(buttonValue);
+            console.log(inputs);
+        } else {
+            // replace operator if the user presses it again
+            inputs[1] = buttonValue;
+        }
+        result = 0;
+    }
+
+    function operate(values){
+        let output = 0;
+        if (values[1] === "+"){
+            output = add(values[0], values[2]);
+            values = [output];
+            return output;
+        } else if (values[1] === "-"){
+            output = subtract(values[0], values[2]);
+            values = [output];
+            return output;
+        } else if (values[1] === "*"){
+            output = multiply(values[0], values[2]);
+            values = [output];
+            return output;
+        } else if (values[1] === "/"){
+            output = divide(values[0], values[2]);
+            values = [output];
+            return output;
+        }
+    }
+    function add(op1, op2){
+        return op1 + op2;
+    }
+
+    function subtract(op1, op2){
+        return op1 - op2;
+    }
+    
+    function divide(op1, op2){
+        return op1 / op2;
+    }
+
+    function multiply(op1, op2){
+        return op1 * op2;
+    }
 })(window, document);
